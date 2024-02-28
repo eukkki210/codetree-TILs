8def1cf75e1efa43ba3cad39499833d8255c2cb5
Seunggi Ryu <68881950+eukkki210@users.noreply.github.com>
2024-02-28 17:40:43 +0900

    Title: 특정 위치의 문자 2, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240228/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        char answer = 0;
+        char[] arr = new char[10];
+
+        String str = sc.nextLine();
+  
+        System.out.println(str.charAt(2) + " " + str.charAt(8) + " " + str.charAt(14));
+    }
+}
