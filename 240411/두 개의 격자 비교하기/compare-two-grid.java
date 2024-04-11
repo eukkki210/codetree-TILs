39a44b28dd7db5a25ee4ace85dc6b5e4abfe1e71
Seunggi Ryu <68881950+eukkki210@users.noreply.github.com>
2024-04-11 17:20:35 +0900

    Title: 두 개의 격자 비교하기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240411/두 개의 격자 비교하기/compare-two-grid.java
+++ 240411/두 개의 격자 비교하기/compare-two-grid.java
@@ -25,7 +25,7 @@ public class Main {
 
         // 출력
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 if (arr1[i][j] == arr2[i][j]) {
                     System.out.print(0 + " ");
                 } else {
