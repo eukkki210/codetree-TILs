93b33e6faa863e69ec28a4f0eb70ac85b86e0e4a
Seunggi Ryu <68881950+eukkki210@users.noreply.github.com>
2024-04-20 15:32:14 +0900

    Title: 격자 반대로 채우기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240420/격자 반대로 채우기/grid-anti-fill.java
+++ 240420/격자 반대로 채우기/grid-anti-fill.java
@@ -3,34 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // n 입력 받기
+
+        // 격자의 크기 n 입력 받기
         int n = scanner.nextInt();
-        
-        int[][] square = new int[n][n]; // n * n 크기의 배열 생성
-        
-        // 정사각형 채우기
-        int num = 1;
-        for (int j = n-1; j >= 0; j--) {
-            if (j % 2 == 0) { // 짝수 열은 아래쪽에서 위쪽으로 증가
-                for (int i = 0; i < n; i++) {
-                    square[i][j] = num++;
+
+        // n x n 크기의 이차원 배열 생성
+        int[][] grid = new int[n][n];
+
+        int num = 1; // 채워넣을 숫자 초기값 설정
+
+        // 오른쪽 아래에서부터 시작하여 위아래로 번갈아가면서 숫자를 채워넣음
+        for (int i = n - 1; i >= 0; i--) {
+            if ((n - i) % 2 == 1) { // 번갈아가면서 위에서 아래로 채움
+                for (int j = n - 1; j >= 0; j--) {
+                    grid[j][i] = num++;
                 }
-            } else { // 홀수 열은 위쪽에서 아래쪽으로 증가
-                for (int i = n - 1; i >= 0; i--) {
-                    square[i][j] = num++;
+            } else { // 번갈아가면서 아래에서 위로 채움
+                for (int j = 0; j < n; j++) {
+                    grid[j][i] = num++;
                 }
             }
         }
-        
-        // 출력
+
+        // 결과 출력
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(square[i][j] + " ");
+                System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }
-        
+
         scanner.close();
     }
 }
