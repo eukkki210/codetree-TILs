66ca6af5e48b27c110f9e52cb5911f760b846f09
Seunggi Ryu <68881950+eukkki210@users.noreply.github.com>
2024-04-30 17:38:06 +0900

    Title: 합과 평균의 차, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240430/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum = a + b + c;
+        int avrg = sum / 3;
+        int sum_minus_avrg = sum - avrg;
+
+        System.out.println(sum);
+        System.out.println(avrg);
+        System.out.println(sum_minus_avrg);
+    }
+}
