751637e67202922ea1bb003847b56c9cc5d8d5c0
Seunggi Ryu <68881950+eukkki210@users.noreply.github.com>
2024-05-10 16:59:51 +0900

    Title: 체질량지수, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240510/체질량지수/bmi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int h = scanner.nextInt();
+        int w = scanner.nextInt();
+        
+        double bmi = (double) w / ((double) h / 100 * (double) h / 100);
+        
+        int roundedBMI = (int) Math.round(bmi * 10) / 10;
+        
+        System.out.println(roundedBMI);
+        
+        if (roundedBMI > 25) {
+            System.out.println("Obesity");
+        }
+    }
+}
